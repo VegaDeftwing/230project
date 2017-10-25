b0a85e346e89f5bd1d1801c421ebde852ad65f96
Vega Deftwing <johnathan698@gmail.com>
2017-10-25 16:40:52 -0500

    add assembler folder and file

---
+++ assembler/assembler.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.6
+print(" ")
+print("-----------------------------------------------------------")
+print("Assembler for CSCE230 project")
+print("Made by Johnathan Carlson, Tyler Zinsmaster, Jake Edinger")
+print("-----------------------------------------------------------")
